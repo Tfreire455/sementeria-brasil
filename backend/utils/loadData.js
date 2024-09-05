c27677c7a4e7b55eb599219a699d6c2c00aa8b91
Tfreire455 <thiago1.000@outlook.com>
2024-09-04 23:13:44 -0300

    atualizacao

--- backend/utils/loadData.js
+++
@@ -1,9 +0,0 @@
-const xlsx = require('xlsx');
-
-const loadData = () => {
-  const workbook = xlsx.readFile("07412046.xlsx");
-  const sheet_name_list = workbook.SheetNames;
-  return xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-};
-
-module.exports = loadData;
